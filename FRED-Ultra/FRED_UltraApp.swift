cbf5c20acc764c64e131ff31c0012b97867e2650
linroger023 <linroger023@uchicago.edu>
2024-12-11 21:12:57 -0800

    Initial Commit

---
+++ FRED-Ultra/FRED_UltraApp.swift
@@ -0,0 +1,17 @@
+//
+//  FRED_UltraApp.swift
+//  FRED-Ultra
+//
+//  Created by Roger Lin on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FRED_UltraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FRED-UltraTests/FRED_UltraTests.swift
@@ -0,0 +1,17 @@
+//
+//  FRED_UltraTests.swift
+//  FRED-UltraTests
+//
+//  Created by Roger Lin on 12/11/24.
+//
+
+import Testing
+@testable import FRED_Ultra
+
+struct FRED_UltraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
